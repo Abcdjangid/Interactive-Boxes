3cd2a35e38ae9de2229e4a29a934ec9a203859a0
chitvan <chitvanjangid27@gmail.com>
2025-02-21 09:49:12 +0530

    cards

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // current dropdown and indicator value.
+    let activeDropdown = null; 
+    let activeIndicator = null;
+
     const items = document.querySelectorAll(".item");
 
-    items.forEach((item) => {
+    // using for each to run loop of each items.
+    items.forEach(function (item) {
         const topItem = item.querySelector(".top-item");
         const dropdown = item.querySelector(".dropdown-container");
+        const indicator = item.querySelector(".indicator");
 
         topItem.addEventListener("click", function () {
-            document.querySelectorAll(".dropdown-container").forEach((drop) => {
-                if (drop !== dropdown) {
-                    drop.style.display = "none";
-                }
-            });
+            if (activeDropdown && activeDropdown !== dropdown) {
+                activeDropdown.style.display = "none";
+                activeIndicator.style.backgroundColor = ""; 
+            }
 
-            dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
+            if (dropdown.style.display === "block") {
+                dropdown.style.display = "none";
+                indicator.style.backgroundColor = "";
+                activeDropdown = null;
+                activeIndicator = null;
+            } else {
+                dropdown.style.display = "block";
+                indicator.style.backgroundColor = "black";
+                activeDropdown = dropdown;
+                activeIndicator = indicator;
+            }
         });
     });
 });
+
